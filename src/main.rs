d8133e5b073d18e70fddff293108563885ce2fff
xiaomuins <1191515708@qq.com>
2020-04-24 00:51:09 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+//    变量绑定
+    let x = 5;
+    println!("{}", x);
+
+    let x = 100;
+    let y = 200;
+    println!("{}+{} = {}",x, y, x + y);
+
+    let client = Client::new();
+
+    let res = client.get("http://example.domain").send().unwrap();
+    assert_eq!(res.status, hyper::Ok);
+
 }
